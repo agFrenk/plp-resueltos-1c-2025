63519ddd8ab70312992e90e816e20f67d27557e0
Agus <afus.frenkel@gmail.com>
2025-04-12 12:14:04 -0300

    avanzamos con el ejercicio 3

--- funcionesUtiles.el
+++ funcionesUtiles.el
@@ -8,11 +8,3 @@
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((haskell . t)))
-
-  (defun replace-results-line ()
-  "Reemplaza '#+RESULTS:' por '*RESULTS:*' en el buffer actual."
-  (interactive)
-  (save-excursion
-    (goto-char (point-min))
-    (while (search-forward "#+RESULTS:" nil t)
-      (replace-match "*RESULTS:*" nil nil))))
